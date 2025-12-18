280a12be9581c698927ab1a2dce7262f58d0560b
teoperalta <teo.peralta.2007@gmail.com>
2025-12-18 12:30:38 -0500

    Problema 05

--- Taller10/src/paquete5/Problema05.java
+++ Taller10/src/paquete5/Problema05.java
@@ -10,5 +10,33 @@ package paquete5;
  * @author reroes
  */
 public class Problema05 {
-    
+
+    public static void main(String[] args) {
+
+        String[][] estudiantes = {{"Sarah Ward", "Philip Payne"},
+        {"Carrie Burton", "Lauren Rice"},
+        {"Emma Escobar", "Lori Flores"},
+        {"Steven West", "Toni Martin"}
+        };
+        String mensaje = "";
+        
+        for (int fila = 0; fila < estudiantes.length;fila++){
+            for (int columna = 0; columna < estudiantes[fila].length;columna++){
+                String nombreCompleto = estudiantes[fila][columna];
+                String inicial = nombreCompleto.substring(0, 1);
+                switch (inicial){
+                    case "S":
+                    case "P":
+                    case "T":
+                        mensaje = mensaje + String.format("-%s\n", nombreCompleto);
+                        break;
+                    default: 
+                        break;
+                        
+                }
+            }
+        }
+          System.out.println(mensaje);
+    }
+
 }
