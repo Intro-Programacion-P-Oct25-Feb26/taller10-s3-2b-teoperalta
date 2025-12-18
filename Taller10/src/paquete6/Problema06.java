264c02428dea26ead7e1bfd4cb4b053031ce5cfc
teoperalta <teo.peralta.2007@gmail.com>
2025-12-18 15:06:14 -0500

    Problema 06

--- Taller10/src/paquete6/Problema06.java
+++ Taller10/src/paquete6/Problema06.java
@@ -8,26 +8,48 @@ package paquete6;
 import java.util.Scanner;
 
 /**
+ * Resolucion parcial del taller
  *
  * @author reroes
  */
 public class Problema06 {
-    
+
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int[][]  matriz = new int [5][6];
-        int[][] matrizResultante = new int [5][6];
+        int[][] matriz = new int[5][6];
+        int[][] matrizResultante = new int[5][6];
         int valor;
-        
-        System.out.println("Ingrese el valor entero:");
+
+        System.out.print("Ingrese el valor entero: ");
         valor = entrada.nextInt();
-        for (int fila = 0; fila < matriz.length;fila++){
-            for (int columna = 0; columna < matriz[fila].length;columna++){
-                System.out.println("Ingrese el  valor para la matriz");
+
+        for (int fila = 0; fila < matriz.length; fila++) {
+            for (int columna = 0; columna < matriz[fila].length; columna++) {
+                System.out.printf("Ingrese valor para [%d][%d]: ", fila, columna);
                 matriz[fila][columna] = entrada.nextInt();
             }
         }
-        for 
-    }
 
+        // Cálculo de la matriz resultante
+        for (int fila = 0; fila < matriz.length; fila++) {
+            for (int columna = 0; columna < matriz[fila].length; columna++) {
+                matrizResultante[fila][columna] = matriz[fila][columna] * valor;
+            }
+        }
+        System.out.println("\n\t--- Matriz ---");
+        for (int fila = 0; fila < matriz.length; fila++) {
+            for (int columna = 0; columna < matriz[fila].length; columna++) {
+                System.out.printf("%4d ", matriz[fila][columna]);
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n  --- Matriz Resultante ---");
+        for (int fila = 0; fila < matrizResultante.length; fila++) {
+            for (int columna = 0; columna < matrizResultante[fila].length; columna++) {
+                System.out.printf("%4d ", matrizResultante[fila][columna]);
+            }
+            System.out.println();
+        }
+    }
 }
