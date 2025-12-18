e1adfce465279bc263959f8a696773728488e608
teoperalta <teo.peralta.2007@gmail.com>
2025-12-18 12:00:35 -0500

    Problema 03

--- Taller10/src/paquete3/Problema03.java
+++ Taller10/src/paquete3/Problema03.java
@@ -10,5 +10,43 @@ package paquete3;
  * @author reroes
  */
 public class Problema03 {
-    
+
+    public static void main(String[] args) {
+
+        boolean[][] sensores = {
+            {true, true, true, true, true, true},
+            {true, false, true, false, true, false},
+            {false, false, false, false, false, false},
+            {true, true, false, true, true, false}
+        };
+        String mensaje = "---------Reporte sensores--------\n";
+        int [] funcional = new int [sensores.length];
+        int [] nofuncional = new int [sensores.length];
+        for (int fila = 0; fila < sensores.length; fila++) {
+            int contadorFuncional = 0;
+            int contadorNoFuncional = 0;
+            for (int columna = 0; columna < sensores[fila].length; columna++) {
+                   if (sensores[fila][columna] == true){
+                       contadorFuncional = contadorFuncional + 1;
+                   }else{
+                       contadorNoFuncional = contadorNoFuncional + 1;
+                   }
+            }
+                   funcional[fila]= contadorFuncional;
+                   nofuncional[fila] = contadorNoFuncional;          
+        }
+          int contador = 1;
+        for (int i = 0; i < 4; i++){
+          
+            if (funcional[i] == 6){
+                mensaje = mensaje + String.format("Zona %d: SIN FALLOS\n",contador);
+            }else{
+                mensaje = mensaje + String.format("Zona %d: %d FALLOS\n", contador, 
+                        nofuncional[i]);
+            }
+            contador++;
+        }
+        System.out.printf(mensaje);
+    }
+
 }
