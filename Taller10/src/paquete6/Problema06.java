9a6d42b649e56b11125db36ae50f9f9c04d590e7
teoperalta <teo.peralta.2007@gmail.com>
2025-12-18 12:46:12 -0500

    Ejercicio 06

--- Taller10/src/paquete6/Problema06.java
+++ Taller10/src/paquete6/Problema06.java
@@ -5,10 +5,29 @@
  */
 package paquete6;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema06 {
+    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int[][]  matriz = new int [5][6];
+        int[][] matrizResultante = new int [5][6];
+        int valor;
+        
+        System.out.println("Ingrese el valor entero:");
+        valor = entrada.nextInt();
+        for (int fila = 0; fila < matriz.length;fila++){
+            for (int columna = 0; columna < matriz[fila].length;columna++){
+                System.out.println("Ingrese el  valor para la matriz");
+                matriz[fila][columna] = entrada.nextInt();
+            }
+        }
+        for 
+    }
 
 }
